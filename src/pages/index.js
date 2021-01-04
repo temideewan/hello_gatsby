7c33b1a4426d77a4ed136c8a4e7a4345ef7e96bd
temidayo omoyajowo <temideewan@gmail.com>
2021-01-04 14:28:45 +0100

    fix import statements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {Link} from 'gatsby';
-import Header from '../components/header';
+import Header from '../components/Header';
 
 export default function Home() {
   return (<div style={{color:'#000123',}}>
